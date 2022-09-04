9c6d595cd608a968ea44d625d61455b6817a6496
Allen <allenanands@gmail.com>
2022-09-04 14:38:08 +0530

    5

--- house/house/urls.py
+++ house/house/urls.py
@@ -22,5 +22,4 @@ from django.views.static import serve
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('scoreboard.urls')),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
